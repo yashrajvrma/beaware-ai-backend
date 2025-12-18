448f1194b18a94cfa14ee65343bd4226ce5a3850
yashrajvrma <vermayashraj2005@gmail.com>
2025-12-18 13:23:51 +0530

    first checkpoint

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,15 +1,15 @@
-import { PrismaPg } from "@prisma/adapter-pg"
-import { PrismaClient } from "@prisma/client"
-import dotenv from "dotenv"
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL,
-})
+  connectionString: process.env.DATABASE_URL,
+});
 
 const prisma = new PrismaClient({
-    adapter
-})
+  adapter,
+});
 
 export default prisma;
