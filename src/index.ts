dad0fc8cb5f5c78aeeebbd3d2445d457b5c990f2
yashrajvrma <vermayashraj2005@gmail.com>
2025-12-18 12:35:53 +0530

    fix: cors error

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"], // Ensure needed methods are allowed
     allowedHeaders: ["Content-Type", "Authorization"], // Allow necessary headers
